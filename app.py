0bb0bcbcad3b08792b39579f77bddb1d70fc94e0
Nicholas <nick.orsi1992@gmail.com>
2023-12-08 12:13:05 -0800

    Initialize start files.

---
+++ app.py
@@ -0,0 +1,24 @@
+"""Flask app for adopt app."""
+
+import os
+
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///adopt")
+
+connect_db(app)
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
