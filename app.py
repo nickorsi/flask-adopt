08d17903a6c77ca159677a539bdcf26eec7eb6b3
Nicholas <nick.orsi1992@gmail.com>
2023-12-08 12:35:09 -0800

    Added seed file, finished creating the Pet class in the model file, and added some more imports to set up the app file.

--- app.py
+++ app.py
@@ -11,8 +11,14 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
 
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False #---> Switch for redirect pause page, False=off True=on
+app.debug = True #---> Switch for debug toolbar, False=off True=on
+debug = DebugToolbarExtension(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", "postgresql:///adopt")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #---> When using SQL Alchemy
+app.config['SQLALCHEMY_ECHO'] = True #---> When using SQL Alchemy
 
 connect_db(app)
 
--- models.py
+++ models.py
@@ -14,3 +14,48 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+    """Pet."""
+
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    name = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    species = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    photo_url = db.Column(
+        db.String(500),
+        nullable=False,
+        default="",
+    )
+
+    age = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    notes = db.Column(
+        db.String(500),
+        nullable=False,
+        default="No Notes",
+    )
+
+    available = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True,
+    )
+
