5bc7cee45c3a2a3c93c39e3cdb61bde6b70678f2
Nicholas <nick.orsi1992@gmail.com>
2023-12-08 15:55:18 -0800

    Added form to add pets.

--- forms.py
+++ forms.py
@@ -1,1 +1,35 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SelectField
+from wtforms.validators import InputRequired, URL, Optional
+
+class AddPet(FlaskForm):
+    "Form for adding pets."
+
+    name = StringField(
+        "Pet Name",
+        validators=[InputRequired()],
+    )
+
+    species = StringField(
+        "Pet Species",
+        validators=[InputRequired()],
+    )
+
+    photo_url = StringField(
+        "Pet Photo",
+        validators=[Optional(), URL()],
+    )
+
+    age = SelectField(
+        "Pet Age",
+        choices=[
+            ('baby','Baby'), ('young','Young'), ('adult','Adult'),
+            ('senior','Senior')
+        ],
+        validators=[],
+    )
+
+    notes = SelectField(
+        "Pet Notes",
+    )
